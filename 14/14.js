f9a96262473f25f2f61855637e626a789e8f0562
Alex Richardson <alex@alexcr.com>
2020-12-11 18:55:52 -0500

    Day 14

---
+++ 14/14.js
@@ -0,0 +1,31 @@
+let recipes = [3, 7];
+const INPUT = parseInt('030121');
+
+// jesus christ
+
+// let one = 0;
+// let two = 1;
+// let i = 0;
+
+// while (true) {
+//   if (recipes.length > 24773876 && recipes.slice(recipes.length - 6) === INPUT) {
+//     console.log('Part one:', recipes.slice(parseInt(INPUT), parseInt(INPUT) + 10));
+//     return; // 5101271252
+//   }
+//   // const match = recipes.length > 24773876 ? recipes.slice(recipes.length - 16, recipes.length - 10).join('') : '';
+//   // if (match === INPUT) {
+//   //   console.log(recipes.slice(recipes.length - 20).join(''));
+//   //   console.log('Part one:', recipes.slice(recipes.length - 10).join(''));
+//   //   return;
+//   // }
+
+//   const sum = (recipes[one] + recipes[two]).toString().split('');
+//   sum.forEach(s => recipes.push(Number(s)));
+
+//   one = (one + recipes[one] + 1) % recipes.length;
+//   two = (two + recipes[two] + 1) % recipes.length;
+
+//   i++;
+// }
+
+// console.log(recipes.slice(recipes.length - 10).join(''));
